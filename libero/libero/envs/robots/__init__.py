75e90cda7a8250a316a9f32f50a812eb5a8fa54a
ravtscheev <git@ravtscheev.com>
2025-12-02 21:18:49 +0100

    Add MountedUR5e robot class

--- libero/libero/envs/robots/__init__.py
+++ libero/libero/envs/robots/__init__.py
@@ -1,5 +1,6 @@
 from .mounted_panda import MountedPanda
 from .on_the_ground_panda import OnTheGroundPanda
+from .mounted_UR5e import MountedUR5e
 
 from robosuite.robots.single_arm import SingleArm
 from robosuite.robots import ROBOT_CLASS_MAPPING
@@ -8,5 +9,6 @@ ROBOT_CLASS_MAPPING.update(
     {
         "MountedPanda": SingleArm,
         "OnTheGroundPanda": SingleArm,
+        "MountedUR5e": SingleArm,
     }
 )
